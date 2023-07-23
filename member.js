f77dc867a4ebfc1560890a97a0fde43e7a402abf
mateuszstruzik <26182867+mateuszstruzik@users.noreply.github.com>
2023-07-23 17:34:38 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,10 @@
 function skillsMemeber() {
-    var skills = ["HTML", "CSS", "JS"];
-    return skills;
+    var x = document.getElementById("skillsMember");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("skillsMemberButton").innerHTML = "Hide";
+    } else {
+      x.style.display = "none";
+      document.getElementById("skillsMemberButton").innerHTML = "Show";
+    }
 }
